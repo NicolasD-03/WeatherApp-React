4665aa5839953db94142bd0c15c24f2844ded2e6
Nicolas DICK <dick.nicolas57@gmail.com>
2021-11-01 23:03:15 +0100

    feat: Set-Up base app file
    Adding form and weather display into main app file
    
     manage submit form
    and fetching data with custom hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
-import Header from 'Components/Header/Header';
+import Header from 'Components/Header';
+import Form from 'Components/Form';
+import Weather from 'Components/Weather';
+
+import useData from 'Hooks/fetchData-hook';
 
 const App = () => {
+  const cityName = useRef(null);
+  const { weatherData, error, isLoading, search } = useData();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    search(cityName.current.value, 'metric', 'en');
+  };
+
   return (
     <div>
       <Header name="test" />
       <h1>Welcome</h1>
       <div>What a beautiful app</div>
+      <Form onSubmit={handleSubmit} inputValue={cityName} />
+      {isLoading ? <div>Loading...</div> : null}
+      {weatherData ? <Weather data={weatherData} /> : null}
+      {error ? <div>{error}</div> : null}
     </div>
   );
 };
